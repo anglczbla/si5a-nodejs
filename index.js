d69f09da80aa432ecf6cd3e12c3e5e97e73253ca
anglczbla <angelicaizabella90@gmail.com>
2024-09-20 11:11:27 +0700

    push

--- index.js
+++ index.js
@@ -8,10 +8,22 @@ const server = http.createServer((req, res)=>
    // res.write('Selamat pagi');
 
     res.writeHead(200, {'Content-Type': 'application/json'});
+
+    if(req.url === '/dosen'){
+        message ='List Data Dosen';
+        data =['Ahmad','Hafiz','Widi'];
+    }else if(req.url === '/mahasiswa'){
+        message='List Data Mahasiswa';
+        data=['Kenny','Rizky','Akbar'];
+    }else{
+        message ='Tidak ditemukan';
+        data= [];
+    }
     res.write(JSON.stringify({
-        'message' : 'selamat pagi',
+        'message' : message,
+        'data' : data,
         'status' : 'success'
-    }))
+    }));
     res.end();
 });
 
